fdd6d9b0deedfe7e5907492c0c5d1396a0107d20
Yunseong <dev.yunseong@gmail.com>
2025-11-18 10:40:04 +0900

    Add README and solution for 컨트럴 제트 problem

---
+++ src/main/java/programmers/ctrlz/Solution.java
@@ -0,0 +1,18 @@
+package programmers.ctrlz;
+
+class Solution {
+    public int solution(String s) {
+        String[] values = s.split(" ");
+        int answer = 0;
+        int lastValue = -1;
+        for (var value : values) {
+            if (value.equals("Z")) {
+                answer -= lastValue;
+            } else {
+                lastValue = Integer.parseInt(value);
+                answer += lastValue;
+            }
+        }
+        return answer;
+    }
+}
